6a1c85565cead64835e180f42ac6888126265c0a
Clarisse Blanco <clarisse.blanco@epita.fr>
2020-09-29 15:05:00 +0200

    made a cleaner version of grayscale

--- IB_clarisse/grayscale.c
+++ IB_clarisse/grayscale.c
@@ -1,19 +1,11 @@
 #include <err.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include "grayscale.h"
 #include "pixel_functions.h"
 
-int main()
+int toGrayscale(SDL_Surface *image_surface)
 {
-  //iniatializing sdl
-  //if cannot, returns 1
-  if(SDL_Init(SDL_INIT_VIDEO) == -1)
-    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
-
-  SDL_Surface* image_surface;
-
-  image_surface = IMG_Load("test_image.jpg");
-  
   //applying grayscale
   
   //code for each pixel of the image
---
+++ IB_clarisse/grayscale.h
@@ -0,0 +1,6 @@
+#ifndef GRAYSCALE_H_
+#define GRAYSCALE_H_
+
+int toGrayscale(SDL_Surface *image_surface);
+
+#endif

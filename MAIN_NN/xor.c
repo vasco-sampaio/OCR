2309d0af804f5ba61a73502b9f20ea4935806f91
thibault <thibault44210@gmail.com>
2020-11-20 19:24:02 +0100

    fix output determination

--- MAIN_NN/xor.c
+++ MAIN_NN/xor.c
@@ -13,7 +13,7 @@ double traininginputs[8] = { 0.0f,0.0f,1.0f,0.0f,0.0f,1.0f,1.0f,1.0f };
 double trainingoutputs[4] = { 0.0f,1.0f,1.0f,0.0f };
 double *trIn = traininginputs;
 double *trOut = trainingoutputs;
-char alphaNum[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char alphaNum[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 char *aNum = alphaNum;
 
 /*
@@ -68,6 +68,6 @@ int main()
 		printf("\n");
 	}
 	neuralNetwork nn = initNN(IMG_SIZE,16,NN_OUT_SIZE);
-	train(nn,inputs,outputs,set_size,10000,1,aNum);
+	train(nn,inputs,outputs,set_size,100000,0.1,aNum);
 	return 0;
 }

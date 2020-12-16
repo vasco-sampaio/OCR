b6e2e9e6468a44003877e0298fad0caac6328213
Renji <dorian.peron@epita.fr>
2020-12-17 00:05:44 +0100

    created test neural nets

--- types.h
+++ types.h
@@ -8,11 +8,11 @@
 
 
 
-# define LEARNING_RATE 0.1
+# define LEARNING_RATE 0.3
 
 # define IMG_SIDE 30
 # define NN_INPUTS IMG_SIDE*IMG_SIDE
-# define NN_HIDDENS 40
+# define NN_HIDDENS 200
 # define NN_OUTPUTS 62
 
 //------------------------------------------------------------------------------

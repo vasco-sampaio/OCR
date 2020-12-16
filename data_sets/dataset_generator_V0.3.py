65ab20bf36b81180cf57bb973d64465591766806
Dorian PERON <dorian.peron@epita.fr>
2020-12-16 20:56:34 +0100

    plif plouf be mechant it's pas ouf

--- data_sets/dataset_generator_V0.3.py
+++ data_sets/dataset_generator_V0.3.py
@@ -1,36 +1,36 @@
-from PIL import Image, ImageDraw, ImageFont, ImageOps
-import os
-import sys
-
 """
 
-Intellectual property of :
+Credits:
 Dorian 'Renji' PERON - dorian.peron@epita.fr
 
 2020
 
 """
 
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import os
+import sys
+
+
 chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def create_letter_image(letter, imgfont, img_size, Xoff = 0, Yoff = 0, angle = 0):
 
-        # create a white binary image of img_size 41*41 
-        image = Image.new("RGB", (img_size, img_size), 'rgb(255,255,255)')
-        img_draw = ImageDraw.Draw(image)
+    # create a white binary image of img_size 41*41 
+    image = Image.new("RGB", (img_size, img_size), 'rgb(255,255,255)')
+    img_draw = ImageDraw.Draw(image)
 
-        (imX, imY) = image.size
-        (tX, tY) = img_draw.textsize(letter, imgfont)
+    (imX, imY) = image.size
+    (tX, tY) = img_draw.textsize(letter, imgfont)
 
-        img_draw.text(((imX - tX)/2 + Xoff,(imY - tY)/2 + Yoff), letter, font = imgfont, fill = 'rgb(0,0,0)')
+    img_draw.text(((imX - tX)/2 + Xoff,(imY - tY)/2 + Yoff), letter, font = imgfont, fill = 'rgb(0,0,0)')
 
-        if angle == 0:
-                return image
-        else:
-                var = image.rotate(angle, fillcolor = 'rgb(255,255,255)')
+    if angle == 0:
+        return image
+    var = image.rotate(angle, fillcolor = 'rgb(255,255,255)')
 
-                return var
+    return var
 
 
 
@@ -40,71 +40,38 @@ def create_letter_image(letter, imgfont, img_size, Xoff = 0, Yoff = 0, angle = 0
 # -> then add extension
 #
 def get_name(c, extension):
-        s = c
-        return s + extension
-       
+    s = c
+    return s + extension
 
 
-def create_datasets(path, fontpath, img_size, font_size = None, offsets = [], rotations = []):
 
-        if not font_size:
-                font_size = img_size // 2 + 2
+def create_datasets(path, fonts, img_size):
+
+    for (fontpath, font_size) in fonts:
+
+        #font_size = img_size // 2 + 2
         img_font = ImageFont.truetype(fontpath, size = font_size)
+        ft_name = fontpath.split('.')[0]
 
         for c in chars:
+            im_name = get_name(c, '_'+ft_name+".bmp")
+            im = create_letter_image(c, img_font, img_size)
+            f = open(os.path.join(path, im_name), "wb")
 
-                im_name = get_name(c, ".bmp")
-                im = create_letter_image(c, img_font, img_size)
-                f = open(os.path.join(path, im_name), "wb")
-
-                im.save(f, "bmp")
-                f.close()
-
-        if offsets != []:
-
-                # offsetted
-                for c in chars:
-                        for (i,j) in offsets:
-
-                                im_name = get_name(c, "_X{0}Y{1}.bmp".format(i,j))
-
-                                im = create_letter_image(c, img_font, img_size, Xoff = i, Yoff = j)
-
-                                f = open(os.path.join(path, im_name), "wb")
-                                im.save(f, "bmp")
-                                f.close()
-
-        if rotations != []:
-                # rotated
-                for c in chars:
-                        for theta in rotations:
-
-                                im_name = get_name(c, "_R{}.bmp".format(theta))
-
-                                im = create_letter_image(c, img_font, img_size, Xoff = 0, Yoff = 0, angle = theta)
-                                im.save(os.path.join(path, im_name))
-
+            im.save(f, "bmp")
+            f.close()
 
 # Where to save the images
 folder = "arial_25_train"
 
-# path to the font file 
-ft_path = "arial.ttf"
-# font size
-f_size = 25
+# path to the font file
+fonts = [("arial.ttf", 30)]
 
 # Image size
 default_size = 30
-# Additional offsetted values
-default_offsets = [(1,1),(-1,1),(1,-1),(-1,-1)]
-# Additional offsetted 
-default_angles = [-10,10]
 
 if not os.path.exists(folder):
-        os.makedirs(folder)
+    os.makedirs(folder)
 
-create_datasets(folder, ft_path, default_size, font_size = f_size, offsets=default_offsets, rotations=default_angles)
+create_datasets(folder, fonts, default_size)
 
-print("Created a dataset with :\n- 1 normal set")
-print("- {} shifted sets".format(len(default_offsets)))
-print("- {} rotated sets".format(len(default_angles)))
